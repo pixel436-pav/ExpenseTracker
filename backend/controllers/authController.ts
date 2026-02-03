cbc991928843040f5a75b521ec367f4f04c82288
pavneetSingh <pavneets436@gmail.com>
2026-02-04 02:58:29 +0530

    Create new account function added

---
+++ backend/controllers/authController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js'
+
+// this function will generate a jwt token 
+const generateToken = (id: string): string => {
+    // logic- to generate a token containing User Id
+
+    return jwt.sign({ id }, // payload : Data You want to store 
+        process.env.JWT_SECRET as string, // Secret : Used to sign in 
+        { expiresIn: '30d' }) // token expires in 30d
+    //jwt.sign (payload,secret,options)
+
+}
+
+// register - Create New User account 
+
+export const registerUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email, password } = req.body
+
+        // Check if the user already exists 
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(404).json({ message: 'User Already Exists' })
+        }
+
+        // below step will create a user - trigger mongoose call the function and store eveything in database
+        const user = await User.create({ name, email, password })
+
+        // below will generate a jwt token 
+         const token = generateToken(user._id.toString())
+
+
+        // will send a response conatinaing token 
+        res.status(201).json({
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            token : token // generatedtoken
+            
+
+        })
+       
+    } catch (error) {
+        res.status(500).json({message:'Server Error'})
+    }
+
+};
+
+// export const loginUser = async (req:Request,res:Response) => {
+//   try {
+//     const {email,password} = req.body;
+//     // find user by email 
+//     const user = await User.findOne({email});
+//     if (user && (await user.matchPassword(password)))
+//   } catch (error) {
+    
+//   }
+// }
+
